c2954359029342efc9f9ab88e22a0f0e01558cea
aryanseth9795 <99879650+aryanseth9795@users.noreply.github.com>
2025-07-17 18:46:14 +0530

    Landing page Mobile View fixed

--- src/app/_component/Landing.jsx
+++ src/app/_component/Landing.jsx
@@ -4,9 +4,9 @@ import { ManPic } from "./Landing_logo";
 
 const Landing = () => {
   return (
-    <div >
-      <section className="bg-white lg:grid lg:h-[10h] lg:place-content-centerdark:bg-gray-900">
-        <div className="mx-auto w-screen max-w-screen-xl px-4 py-1 sm:px-6 sm:py-8 md:grid md:grid-cols-2  md:gap-4  lg:px-8 lg:py-20">
+    <div className="flex flex-col  w-screen">
+      <section className="bg-white lg:grid lg:h-[75vh] lg:place-content-center dark:bg-gray-900 ">
+        <div className=" w-screen max-w-screen-xl  py-1 sm:px-6 sm:py-8 md:grid md:grid-cols-2  md:gap-4  lg:px-8 lg:py-20">
           <div className="max-w-prose text-left">
             <h1 className="text-4xl font-bold text-gray-900 sm:text-5xl dark:text-white">
               <strong className="text-indigo-600">24/7 </strong><br/>
@@ -18,7 +18,7 @@ const Landing = () => {
              News That Knows Where You Are
             </h1>
 
-            <div className="mt-4 flex gap-4 sm:mt-6 justify-start m-10">
+            <div className="mt-4 flex gap-4 sm:mt-6 justify-center md:justify-start m-10">
               <a
                 className="inline-block rounded border border-indigo-600 bg-indigo-600 px-5 py-3 font-medium text-white shadow-sm transition-colors hover:bg-indigo-700"
                 href="/news"
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,12 +4,12 @@ import Header from "./_component/Header";
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col h-screen w-screen">
 
       <Header
         style={{ position: "static", top: 0, left: 0, right: 0, zIndex: 10 }}
       />
-      <div className="flex h-screen  p-25 ">
+      <div className="flex h-[75vh] pt-20">
         <Landing />
       </div>
 
